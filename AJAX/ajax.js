a04d7157b0b093892bd488910dac600fdd91461c
arunkudiyal <arunkudiyal@icloud.com>
2026-09-09 15:54:01 +0530

    Basics of AJAX - GitHub API

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -4,6 +4,7 @@ const url = 'https://api.github.com/users'
 console.log(xhr.readyState) // 0
 
 // s2 --> open a portal of communication b/w client & server
+// xhr.open('requestMethod', 'url')
 xhr.open('GET', url)
 console.log(xhr.readyState) // 1
 
@@ -13,9 +14,20 @@ xhr.onreadystatechange = () => {
     console.log(xhr.readyState) // 2 --> 3 --> 4
     if (xhr.readyState == 4) {
         const resObj = JSON.parse(xhr.responseText)
+        let output = ''
         for (let i = 0; i < resObj.length; i++) {
-            console.log(resObj[i].avatar_url)
+            output += `
+                <div>
+                    <img style="height: 500px; width: 500px;" src="${resObj[i].avatar_url}" />
+                    <br />
+                    <h2>${resObj[i].login}</h2>
+                    <a href="${resObj[i].html_url} target="_blank ">Visit GitHub</a>
+                </div>  
+                <hr />
+            `
         }
+        console.log(output)
+        document.getElementById('main').innerHTML = output
     }
 }
 
