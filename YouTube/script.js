2638fb0be90caa4a31c7ef14a28741dbe2a79517
arunkudiyal <arunkudiyal@icloud.com>
2026-09-18 11:15:37 +0530

    YouTube App

--- YouTube/script.js
+++ YouTube/script.js
@@ -1,27 +1,24 @@
-const box = document.getElementById('search')
 const btn = document.getElementById('btn')
 
 btn.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    // API call
-    const xhr = new XMLHttpRequest();
+    const search = document.getElementById('searchBox').value
 
-    const apiKey = 'AIzaSyDbZVXGe6CUjfF_4IroHTPQh-9I0Ouf914'
-    const searchValue = box.value
+    const xhr = new XMLHttpRequest()
+    const apiKey = 'AIzaSyBmnGj6x4ijIuvJPlNbDaRaH2QpEIlATTA'
 
-    const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&q=${searchValue}`
+    const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&q=${search}`
 
+    // Call the API
     xhr.open('GET', url)
 
     xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            let response = JSON.parse(xhr.responseText)
-            console.log(response);
+        if (xhr.status == 200 && xhr.readyState == 4) {
+            const response = JSON.parse(xhr.responseText)
+            console.log(response)
         }
     }
 
     xhr.send()
 })
-
-
